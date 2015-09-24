dc3deb4e7d03e7229da0ab5616562bd475b02e92
Richard Dennis <mad_raptor@hotmail.com>
2015-09-24 20:28:15 +0100

    Update poolCompromised.py

--- poolCompromised.py
+++ poolCompromised.py
@@ -1,16 +1,37 @@
 s=set(range(1,153330)) # Set the range - 150000 Non malicious notary nodes - 3330 notary malicious nodes   #Change depending on network size, 3330 always the attack number
+# s=set(range(1, (15000+3330)))
+# s=set(range(1, (1500+3330)))
+# s=set(range(1, (150+3330)))
+# s=set(range(1, (15+3330)))
+
+# s=set(range(1, (150000+143)))
+# s=set(range(1, (15000+143)))
+# s=set(range(1, (1500+143)))
+# s=set(range(1, (150+143)))
+# s=set(range(1, (15+143)))
+
+
 import random
 
-a = [] #Number of times when a malicious pool was selected - 1 gets added to the array each
+v = []
 
-b = 0 
+f = 0
 
-while b < 1000000: #Number of times to run the test
+while f < 50:
+	a = [] #Number of times when a malicious pool was selected - 1 gets added to the array each
 
-	r = random.sample(s,7) #Selects 7 random nodes - (7 is size of the pool)
-	x = sum( i < 3331 for i in r) #Assumes 0 - 3330 nodes are malicious, order does not matter here
-	if x >3:  #If more than 3 nodes 3330 and under are found the pool is compromised
-		a.append(1) #Add to the array 
-	b = b + 1 #Repeat test
+	b = 0 
 
-print sum(a) #Number of times a compromised pool is selected
+	while b < 1000000: #Number of times to run the test
+
+		r = random.sample(s,7) #Selects 7 random nodes - (7 is size of the pool)
+		x = sum( i < 3331 for i in r) #Assumes 0 - 3330 nodes are malicious, order does not matter here
+		if x >3:  #If more than 3 nodes 3330 and under are found the pool is compromised
+			a.append(1) #Add to the array 
+		b = b + 1 #Repeat test
+
+	print sum(a) #Number of times a compromised pool is selected
+	v.append(a)
+	f = f + 1
+
+print v
